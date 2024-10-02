d78ecd37f64ec267d3d87807757c4b7a97ab7bcc
AnanthuMS <ananthu@torcue.com>
2024-10-02 12:21:37 +0530

    url update

--- src/globals/config.js
+++ src/globals/config.js
@@ -1,12 +1,15 @@
 const CONFIG = {
+  // siteUrl: 'https://bidz.ibidz.in/',
   siteUrl: 'https://bidz.ibidz.in/',
+
   // base_url: `http://ec2-18-209-21-42.compute-1.amazonaws.com/ibidzqaapi/`,
   // base_url: ` http://ec2-44-201-125-219.compute-1.amazonaws.com/ibidzqaapi/`,
-  base_url: `https://www.mybidz.in/ibidzqaapi/`,
+  // base_url: `https://www.mybidz.in/ibidzqaapi/`,
+  base_url: `https://api.ibidz.in/`,
 
-  // base_url: `https://api.ibidz.in/`,
-  image_base_url: `https://admin.ibidz.in/`,
+  // image_base_url: `https://admin.ibidz.in/`,
   // image_base_url: `https://www.mybidz.in/webadmin/`,
+  image_base_url: `https://admin.ibidz.in/`,
 };
 
 export default CONFIG;
